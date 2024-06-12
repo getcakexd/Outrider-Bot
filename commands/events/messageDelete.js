7ed4ce448a8b7b5afbece2e784b31bcc1b4dcf4d
getcakexd <cxc72413@gmail.com>
2024-06-12 18:51:23 +0200

    event log fix

--- commands/events/messageDelete.js
+++ commands/events/messageDelete.js
@@ -1,8 +1,16 @@
+const cfg = require("../../config.json");
 module.exports = async (client, message) => {
   const embedBuilder = require("../utils/embeds");
   const cfg = require("../../config.json");
 
-  if (message.guild.id !== cfg.guildId) return;
+  try {
+    if (message.guild.id !== cfg.guildId) return;
+    if (message.author.bot) return;
+  } catch (exeption) {
+      console.log(`[ERROR] An error occurred while managing events`)
+      console.log(`[ERROR] ${exeption}`)
+      console.log(" ")
+  }
 
   await client.channelLogs.messageLog.send({
     embeds: [embedBuilder.messageD(client, message)],
--- commands/events/messageUpdate.js
+++ commands/events/messageUpdate.js
@@ -1,9 +1,17 @@
+
 module.exports = async (client, oldMessage, newMessage) => {
   const embedBuilder = require("../utils/embeds");
   const cfg = require("../../config.json");
 
-  if (newMessage.guild.id !== cfg.guildId) return;
-  if (!oldMessage.author) return;
+  try {
+    if (newMessage.guild.id !== cfg.guildId) return;
+    if (newMessage.author.bot) return;
+    if (!oldMessage.author) return
+  } catch (exeption) {
+    console.log(`[ERROR] An error occurred while managing events`)
+    console.log(`[ERROR] ${exeption}`)
+    console.log(" ")
+  }
 
   await client.channelLogs.messageLog.send({
     embeds: [embedBuilder.messageU(client, oldMessage, newMessage)],
